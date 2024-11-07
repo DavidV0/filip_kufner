e162f8052d68f14fe9a3f5be46bd547bff20943d
YourGitHubUsername <davidvelickovic@hotmail.com>
2024-11-07 13:18:14 +0100

    small changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,25 +8,18 @@ import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-poli
 import { AddComponent } from './components/add/add.component';
 import { GalleryViewComponent } from './components/gallery-view/gallery-view.component';
 import { authGuard } from './guards/auth.guard';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: 'home', component: AboveTheFoldComponent},
-    {path: 'work', component: WorkComponent},
-    {path: 'work/:categoryId', component: WorkComponent},
-    {path: 'work/:categoryId/:subCategoryId', component: GalleryViewComponent},
-    {path: 'about', component: AboutMeComponent},
-    {path: 'connect', component: ConnectComponent},
-    {path: 'imprint', component: ImprintComponent},
-    {path: 'privacy-policy', component: PrivacyPolicyComponent},
-    { 
-        path: 'admin', 
-        loadComponent: () => import('./components/add/add.component').then(m => m.AddComponent),
-        canActivate: [authGuard]
-      },
-      {
-        path: 'login',
-        loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent)
-      },
-     
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: AboveTheFoldComponent },
+  { path: 'work', component: WorkComponent },
+  { path: 'work/:categoryId', component: WorkComponent },
+  { path: 'work/:categoryId/:subCategoryId', component: GalleryViewComponent },
+  { path: 'about', component: AboutMeComponent },
+  { path: 'connect', component: ConnectComponent },
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AddComponent, canActivate: [authGuard] },
 ];
