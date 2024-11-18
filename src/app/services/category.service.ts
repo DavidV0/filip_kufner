730c257817a70f70d67feb13937894ef45f5243a
YourGitHubUsername <davidvelickovic@hotmail.com>
2024-11-18 15:26:34 +0100

    favicon added

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -5,26 +5,28 @@ import { Category } from '../interfaces/category.interface';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
   private categories$: Observable<Category[]>;
 
   constructor(private firestore: Firestore) {
     const categoriesCollection = collection(this.firestore, 'categories');
-    this.categories$ = collectionData(categoriesCollection, { idField: 'id' }) as Observable<Category[]>;
-    this.categories$.subscribe(categories => console.log(categories));
+    this.categories$ = collectionData(categoriesCollection, {
+      idField: 'id',
+    }) as Observable<Category[]>;
+    this.categories$.subscribe((categories) => {
+      // Handle categories as needed, currently no action taken
+    });
   }
 
   getCategories(): Observable<Category[]> {
     return this.categories$;
-    console.log(this.categories$);
-
   }
 
   getCategoryById(id: string): Observable<Category | undefined> {
     return this.categories$.pipe(
-      map(categories => categories.find(category => category.id === id))
+      map((categories) => categories.find((category) => category.id === id))
     );
   }
-} 
+}
