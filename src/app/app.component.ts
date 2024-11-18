52e35f874875fd19de39cfd6a13f7c25a262bb7c
YourGitHubUsername <davidvelickovic@hotmail.com>
2024-11-18 15:05:17 +0100

    added htaccess

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { AboveTheFoldComponent } from './components/above-the-fold/above-the-fold.component';
-import { WorkCollectionComponent } from './components/work-collection/work-collection.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
-import { AboutMeComponent } from './components/about-me/about-me.component';
-import { ConnectComponent } from './components/connect/connect.component';
 import { ScrollToTopService } from './services/scroll-to-top.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    NavbarComponent,
-    NavbarComponent,
-    AboveTheFoldComponent,
-    WorkCollectionComponent,
-    FooterComponent,
-    AboutMeComponent,
-    ConnectComponent,
-  ],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
